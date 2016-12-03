e7fc2dd318f861132fe2f0853ac5057e73e32672
Jimeno0 <carlospj@icloud.com>
2016-12-03 12:02:40 +0100

    Refactor sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
 class SessionsController < ApplicationController
   before_action :current_user, only: [:destroy]
-  def create
 
+  def create
     user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      user.regenerate_token
-      render json: user, status: 200
-    else
+    unless user && user.authenticate(params[:password])
       render json: { error: "invalid user or password" }, status: 400
+      return
     end
+    user.regenerate_token
+    render json: user, status: 200
   end
 
   def destroy
@@ -19,9 +19,6 @@ class SessionsController < ApplicationController
   private
   def current_user
     @user = User.find_by(token: params[:token])
-    unless @user
-      render json: { error: "user not found"}, status: 404
-      return
-    end
+    return  render json: { error: "user not found"}, status: 404 unless @user
   end
 end
