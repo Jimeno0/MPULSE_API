3be9cd9ad03a2f5aa97b75761430efab9245e976
Jimeno0 <carlospj@icloud.com>
2016-12-14 18:26:08 +0100

    Refactor artist controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,9 +4,9 @@ class ArtistsController < ApplicationController
     artists = @user.artists.all
     if artists
       render json: artists, status: 200
-    else
-      render json: {error: "Cant get artists artist" }, status: 400
+      return
     end
+    render json: {error: "Cant get artists artist" }, status: 400
   end
 
   def create
@@ -14,19 +14,21 @@ class ArtistsController < ApplicationController
     if artist
       @user.artists.push(artist)
       render json: artist, status: 201
-    else
-      render json: {error: "impossible to create artist" }, status: 400
+      return
     end
+    render json: {error: "impossible to create artist" }, status: 400
   end
 
   def destroy
     artist = Artist.find_by(name: params[:name].capitalize)
     user_artist = @user.artists.find_by(name: artist.name)
-    return render json: {error: "user dont have this artist as favourite"}, status: 400 unless user_artist
-
-    user_artist.delete if user_artist.users.size <= 1
-    @user.artists.delete(user_artist) if user_artist.users.size > 1
-
-    render json: artist, status: 200
+    if user_artist
+      user_artist.delete if user_artist.users.size <= 1
+      @user.artists.delete(user_artist) if user_artist.users.size > 1
+      render json: artist, status: 200
+      return
+    end
+    render json: {error: "user dont have this artist as favourite"}, status: 400
   end
 end
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,7 @@ class UsersController < ApplicationController
   skip_before_action :current_user, only: [:create]
 
   def create
-
     @user = User.new(user_params)
-
     return render json: @user, status: 201 if @user.save
     render json: { error: "cannot create user"}, status: 400
   end
