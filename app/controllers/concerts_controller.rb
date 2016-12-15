5611dcc1f3a1e78d0c4b327592fe75b1b0623e42
Jimeno0 <carlospj@icloud.com>
2016-12-15 18:06:18 +0100

    Change concerts method

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -29,61 +29,20 @@ class ConcertsController < ApplicationController
 
   def last
     tomorrow = Date.tomorrow.to_s
-    url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=VIxfWLncF71QZ3aoc9OLoeGU9NnAsVRj&startDateTime=#{tomorrow}T00:00:00Z&size=9&classificationName=Music"
-    render TicketmasterApi.handle_ticketmaster_API(url)
+    # url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=VIxfWLncF71QZ3aoc9OLoeGU9NnAsVRj&startDateTime=#{tomorrow}T00:00:00Z&size=9&classificationName=Music"
+    partial_url = "&startDateTime=#{tomorrow}T00:00:00Z&size=9&classificationName=Music"
+    render TicketmasterApi.handle_ticketmaster_API(partial_url)
   end
 
   def search
-    artist = params[:search].gsub(" ","%")
-    url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=VIxfWLncF71QZ3aoc9OLoeGU9NnAsVRj&keyword=#{artist}"
-    render TicketmasterApi.handle_ticketmaster_API(url)
+    artist = params[:search].gsub(" ","%20")
+    # url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=VIxfWLncF71QZ3aoc9OLoeGU9NnAsVRj&keyword=#{artist}"
+    partial_url = "&keyword=#{artist}"
+    render TicketmasterApi.handle_ticketmaster_API(partial_url)
   end
 
   private
   def concert_params
    params.require(:concert).permit(:name, :date, :url, :genre, :subgenre, :country, :lat, :long, :city, :venue, :concert_id, :sale, :image)
   end
-  #
-  # def handle_ticketmaster_API(url)
-  #   response = HTTP.get(url)
-  #   if JSON.parse(response.body)["page"]["totalElements"] == 0
-  #     render json: { error: "No concerts for this artist yet"}, status: 404
-  #     return
-  #   end
-  #
-  #   events = JSON.parse(response.body)["_embedded"]["events"]
-  #   @concerts = []
-  #
-  #   events.each do |event|
-  #     concert = {
-  #       name: event["name"],
-  #       date: event["dates"]["start"]["localDate"],
-  #       url: event["url"],
-  #       genre: event["classifications"][0]["genre"]["name"],
-  #       subgenre: event["classifications"][0]["subGenre"]["name"],
-  #       concert_id: event["id"]
-  #     }
-  #
-  #     if (event["_embedded"]["venues"][0]["country"] && event["_embedded"]["venues"][0]["location"])
-  #       concert["country"] = event["_embedded"]["venues"][0]["country"]["countryCode"]
-  #       concert["lat"] = event["_embedded"]["venues"][0]["location"]["latitude"]
-  #       concert["lon"] = event["_embedded"]["venues"][0]["location"]["longitude"]
-  #       concert["city"] = event["_embedded"]["venues"][0]["city"]["name"]
-  #       concert["venue"] = event["_embedded"]["venues"][0]["name"]
-  #     end
-  #
-  #     sale = event["dates"]["status"]["code"]
-  #     if sale == "onsale"
-  #       concert["sale"] = true
-  #     else
-  #       concert["sale"] = false
-  #     end
-  #     images_array = event["images"]
-  #     images_array.each do |image|
-  #       concert["image"] =  image["url"] if image["ratio"] == "16_9"
-  #     end
-  #     @concerts.push(concert);
-  #   end
-  #   render json: @concerts, status: 200
-  # end
 end
