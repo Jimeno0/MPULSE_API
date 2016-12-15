95699995e6fc98960c0447f828433e85ee7ac866
Jimeno0 <carlospj@icloud.com>
2016-12-15 18:05:32 +0100

    Change ticketmasterAPI model

--- app/models/ticketmaster_api.rb
+++ app/models/ticketmaster_api.rb
@@ -1,22 +1,26 @@
 class TicketmasterApi < ApplicationRecord
-  def self.handle_ticketmaster_API(url)
+  def self.handle_ticketmaster_API(partial_url)
+    api_key = "apikey=VIxfWLncF71QZ3aoc9OLoeGU9NnAsVRj"
+    uri = "https://app.ticketmaster.com/discovery/v2/events.json?"
+    url = "#{uri}#{api_key}#{partial_url}"
+
     response = HTTP.get(url)
     if JSON.parse(response.body)["page"]["totalElements"] == 0
        return {json: { error: "No concerts for this artist yet"}, status: 404}
     end
-
     events = JSON.parse(response.body)["_embedded"]["events"]
     @concerts = []
-
     events.each do |event|
       concert = {
         name: event["name"],
         date: event["dates"]["start"]["localDate"],
         url: event["url"],
-        genre: event["classifications"][0]["genre"]["name"],
-        subgenre: event["classifications"][0]["subGenre"]["name"],
         concert_id: event["id"]
       }
+      if event["classifications"]
+        concert["genre"] = event["classifications"][0]["genre"]["name"]
+        concert["subgenre"] = event["classifications"][0]["subGenre"]["name"]
+      end
 
       if (event["_embedded"]["venues"][0]["country"] && event["_embedded"]["venues"][0]["location"])
         concert["country"] = event["_embedded"]["venues"][0]["country"]["countryCode"]
