3b6b9d34dec9cbd45940dfffdc357e9bfd9aa0c3
Jimeno0 <carlospj@icloud.com>
2016-12-15 11:52:45 +0100

    Update foreginkeys migration

--- db/migrate/20161215104528_update_foregin_keys.rb
+++ db/migrate/20161215104528_update_foregin_keys.rb
@@ -1,6 +1,8 @@
 class UpdateForeginKeys < ActiveRecord::Migration[5.0]
   def change
-    remove_foreign_key :concerts, :artists
-    add_foreign_key :concerts, :artists, on_delete: :cascade
+    remove_foreign_key :concerts, :users
+    remove_foreign_key :artists, :users
+    add_foreign_key :concerts, :users, on_delete: :cascade
+    add_foreign_key :artists, :users, on_delete: :cascade
   end
 end
